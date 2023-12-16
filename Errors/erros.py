25d9c91d6beaeaca67fd6d68670628107bfda342
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2023-12-15 21:36:32 -0300

    erros

---
+++ Errors/erros.py
@@ -0,0 +1,11 @@
+class NotFilled(Exception):
+    def __init__(self, value_error):
+        self.value_error = value_error
+    def __repr__(self) -> str:
+        return self.value_error
+
+class NoCheckboxesFilled(Exception):
+    def __init__(self, value_error):
+        self.value_error = value_error
+    def __repr__(self) -> str:
+        return self.value_error
