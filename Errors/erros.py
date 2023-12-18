d5faac5ee39be2f777218bf57e2e217b656e9bec
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2023-12-18 14:21:38 -0300

    erros

--- Errors/erros.py
+++ Errors/erros.py
@@ -9,3 +9,10 @@ class NoCheckboxesFilled(Exception):
         self.value_error = value_error
     def __repr__(self) -> str:
         return self.value_error
+    
+
+class NoProductsSelected(Exception):
+   def __init__(self, value_error):
+        self.value_error = value_error
+    def __repr__(self) -> str:
+        return self.value_error 
